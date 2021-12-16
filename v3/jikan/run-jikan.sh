263e0bce622d799013ca298f1911df73d91c4646
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-12-16 20:50:27 +0000

    Docker updated

--- v3/jikan/run-jikan.sh
+++ v3/jikan/run-jikan.sh
@@ -3,5 +3,5 @@
 chmod -R a+w storage/
 
 # php:7.3.14-apache-buster ENTRYPOINT
-#docker-php-entrypoint apache2-foreground
-php -S 0.0.0.0:8000 -t public
+# docker-php-entrypoint apache2-foreground
+php -S 127.0.0.1:60080 -t public
