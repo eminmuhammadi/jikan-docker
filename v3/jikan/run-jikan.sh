da827a278ec60b126cf6c2f0b49c7a46a5a84f5e
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-12-24 13:09:12 +0100

    optimization

--- v3/jikan/run-jikan.sh
+++ v3/jikan/run-jikan.sh
@@ -2,6 +2,14 @@
 # allow write permission
 chmod -R a+w storage/
 
+# Optimization
+php artisan config:cache
+php artisan route:cache
+
+php artisan optimize --force
+composer dumpautoload -o
+
+
 # php:7.3.14-apache-buster ENTRYPOINT
 # docker-php-entrypoint apache2-foreground
 php -S 127.0.0.1:60080 -t public
